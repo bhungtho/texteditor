f5cd615398cf58768128bb06dbb496557ecdb4a0
Ben Hung <kopnosema@gmail.com>
2020-06-01 16:09:11 -0500

    disabled raw mode & errors

---
+++ Makefile
@@ -0,0 +1,2 @@
+te: te.c
+	$(CC) te.c -o te -Wall -Wextra -pedantic -std=c99
---
+++ te.c
@@ -0,0 +1,92 @@
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <errno.h>
+
+void enable_raw_mode();
+void disable_raw_mode();
+void die(const char *s);
+
+struct termios orig_termios;
+
+void enable_raw_mode() {
+    // reads terminal attributes into raw
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+        die("tcgetattr");
+    }
+    
+    // automatically call disable_raw_mode when the program exits
+    atexit(disable_raw_mode);
+
+    struct termios raw = orig_termios;
+
+    // local flags
+    // ECHO - turn off echoing
+    // ICANON - turn off canonical mode
+    // ISIG - turn off ctrl-c & ctrl-z
+    // IEXTEN - disable ctrl-v
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+    // input flag
+    // IXON - disable ctrl-s & ctrl-q
+    // ICRNL - fix ctrl-m
+    // BRKINT - stop break conditions
+    // INPCK - disable parity checking
+    // ISTRIP - disables stripping of 8th bit
+    raw.c_iflag &= ~(IXON | ISTRIP | INPCK | ICRNL | BRKINT);
+
+    // output flag
+    // OPOST - turn off output processing (\r\n)
+    raw.c_oflag &= ~(OPOST);
+
+    // CS8 - sets character size to 8 bits per byte
+    raw.c_cflag |= (CS8);
+
+    // time-in/time-out values
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    // sets terminal attributes from raw
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        die("tcsetattr");
+    }
+}
+
+void disable_raw_mode() {
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+        die("tcsetattr");
+    }
+}
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+int main() {
+    enable_raw_mode();
+
+    // reads user input until a q is seen
+    while(1) {
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+            die("read");
+        }
+        
+        // checks if a character is a control character
+        if(iscntrl(c)) {
+            printf("%d\r\n", c);
+        }
+        else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+
+        if(c == 'q') {
+            break;
+        }
+    }
+
+    return 0;
+}
