13f93d78cc47557d16171c134b03a65bf507dc5f
Ben Hung <kopnosema@gmail.com>
2020-06-09 17:13:29 -0500

    comments for text viewer

--- te.h
+++ te.h
@@ -1,4 +1,5 @@
-//defines
+/* defines */
+
 #define VERSION "0.0.1"
 #define TAB_STOP 8
 #define CTRL_KEY(k) ((k) & 0x1f)    // strips top three bits
@@ -9,7 +10,8 @@
 #define _GNU_SOURCE
 #define _SVID_SOURCE
 
-// includes
+/* includes */
+
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
@@ -22,7 +24,9 @@
 #include <time.h>
 #include <stdarg.h>
 
-// enums
+/* enums */
+
+// key presses
 enum editorKey {
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
@@ -35,70 +39,75 @@ enum editorKey {
     PAGE_DOWN
 };
 
-// structure definitions
+/* structure definitions */
+
+// one row of text
 typedef struct editor_row {
-    int size;
-    int r_size;
-    char * chars;
-    char * render;
+    int size;       // size of the row
+    int r_size;     // size of the render
+    char * chars;   // text
+    char * render;  // render text
 } editor_row;
 
+// editor variables
 typedef struct editor_config {
-    int cx;
-    int cy;
-    int rx;
-    int row_offset;
-    int col_offset;
-    int screen_rows;
-    int screen_cols;
-    int num_rows;
-    editor_row  * row;
-    char * file_name;
-    char status_msg[80];
-    time_t status_time;
-    struct termios orig_termios;
+    int cx;                         // cursor x location
+    int cy;                         // cursor y location
+    int rx;                         // index into render
+    int row_offset;                 // what row the user is on
+    int col_offset;                 // column offset
+    int screen_rows;                // number of rows on the screen
+    int screen_cols;                // number of columns of the screen
+    int num_rows;                   // number of rows in the file
+    editor_row  * row;              // array of editor_rows that hold the text
+    char * file_name;               // name of the file
+    char status_msg[80];            // status message
+    time_t status_time;             // status time
+    struct termios orig_termios;    // holds flags for the terminal
 } editor_config;
 
+// to-add to editor
 typedef struct append_buffer {
-    char * b;
-    int len;
+    char * b;       // actual buffer
+    int len;        // length of buffer
 } append_buffer;
 
-// variables
-editor_config E;
+/* variables */
 
-// function declarations
+editor_config E;    // our editor
+
+/* function declarations */
 
 // append buffer
-void ab_append(append_buffer * ab, const char * s, int len);
-void ab_free(append_buffer * ab);
+void ab_append(append_buffer * ab, const char * s, int len);    // add to our buffer
+void ab_free(append_buffer * ab);                               // free buffer
 
 // terminal
-int editor_read_key();         // wait for a keypress and return it
+int editor_read_key();          // wait for a keypress and return it
 void enable_raw_mode();         // enable "raw-mode"
 void disable_raw_mode();        // disable "raw-mode"
 void die(const char *s);        // error function
 int get_window_size(int * rows, int * cols);
 
 // output
-void editor_set_status(const char * fmt, ...);
-void editor_draw_message_bar(append_buffer * ab);
-void editor_draw_status_bar(append_buffer * ab);
-void editor_scroll();
-void editor_refresh_screen();
-void editor_draw_rows();
+void editor_set_status(const char * fmt, ...);      // set status bar message
+void editor_draw_message_bar(append_buffer * ab);   // draw message bar on screen
+void editor_draw_status_bar(append_buffer * ab);    // draw status bar on screen
+void editor_scroll();                               // scroll the screen
+void editor_refresh_screen();                       // refresh the screen
+void editor_draw_rows();                            // draw the rows on the screen
 
 // input
-void editor_move_cursor(int key);
-void editor_process_keypress(); // wait for a keypress and handle it
+void editor_move_cursor(int key);   // move the cursor on the screen
+void editor_process_keypress();     // wait for a keypress and handle it
 
 // row operations
-int cx_to_rx(editor_row * row, int cx);
-void editor_update_row(editor_row * row);
-void editor_append_row(char * s, size_t length);
+int cx_to_rx(editor_row * row, int cx);           // converts cx to rx
+void editor_update_row(editor_row * row);         // updates the specified row
+void editor_append_row(char * s, size_t length);  // appends the row to the screen
 
 // file i/o
-void editor_open();
+void editor_open(); // opens the terminal to the editor
 
 // init
-void init_editor();
+void init_editor(); // initializes the editor
